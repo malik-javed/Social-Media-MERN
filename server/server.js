821c0ebe298bb70c1ceab3c3f584be4196f64d57
malik-javed <malikjaved1234000@gmail.com>
2023-05-30 23:39:08 +0530

    commit 2

--- server/server.js
+++ server/server.js
@@ -55,12 +55,7 @@ mongoose.connect(URI, {
     console.log("By malik")
 })
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+
 
 
 const port = process.env.PORT || 5000
